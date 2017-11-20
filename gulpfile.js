f59f9db30327741073b23bd10791570bf3c08dbf
James <james.neale@whichbingo.co.uk>
2017-11-20 11:35:54 +0000

    Implemented header structure and basic styling

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,6 @@ Gulpfile Setup
 
 
 //Require our dependencies
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-//
-// //Compile our sass into css
-// gulp.task('styles', function() {
-//     gulp.src('sass/main.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('css/'));
-// });
-
-//Setup watch task
-// gulp.task('watch',function() {
-//     gulp.watch(paths.sass + 'sass/**/*.scss',['styles']);
-//     console.log(paths.sass + 'styles/**/*.scss');
-// });
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
